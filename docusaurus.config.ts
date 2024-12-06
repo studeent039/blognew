9ce3cafc1d9423fc8b1aa72f738fa4646f75abc4
studeent039 <156401478+studeent039@users.noreply.github.com>
2024-12-06 20:10:47 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -53,9 +53,9 @@ const config: Config = {
       hideOnScroll: true,
       items: [
         { label: '博客', position: 'right', to: 'blog' },
-        { label: '项目', position: 'right', to: 'project' },
-        { label: '友链', position: 'right', to: 'friends' },
-        { label: '关于', position: 'right', to: 'about' },
+        { label: '項目', position: 'right', to: 'project' },
+        { label: '友鏈', position: 'right', to: 'friends' },
+        { label: '關於', position: 'right', to: 'about' },
         {
           label: '更多',
           position: 'right',
